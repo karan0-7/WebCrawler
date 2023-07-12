8a294c18cd062f4c225fbf1a6107ab225248a2e7
Luc721 <karanguleria07@gmail.com>
2023-07-12 14:30:58 +0530

    Testing

--- crawl.js
+++ crawl.js
@@ -1,3 +1,5 @@
+const {JSDOM} = require("jsdom");
+
 function normalizeURl(url)
 {
   if(url.slice(-1)=="/"){url=url.slice(0,-1)};
@@ -5,4 +7,33 @@ function normalizeURl(url)
   return `${newURl.hostname}${newURl.pathname}`
 }
 
-module.exports = {normalizeURl}
+function getURLsfromHTML(htmlBody,baseURL)
+{
+  let url =[];
+  const dom = new JSDOM(htmlBody);
+ for(let link of dom.window.document.querySelectorAll('a'))
+ {
+  if(link.href.charAt(0)==="/"){
+    try{ new URL(`${baseURL}${link.href}`);
+    url.push(`${baseURL}${link.href}`)
+  }
+  catch(e)
+  {
+    console.log("Relative   ",e.message)
+  }
+  }
+  else{
+    try{
+      new URL(link.href);
+      url.push(link.href);
+    }
+    catch(e)
+    {
+      console.log("Absolute  ",e.message)
+    }
+  }
+ }
+  return url;
+} 
+
+module.exports = {normalizeURl,getURLsfromHTML}
--- crawl.test.js
+++ crawl.test.js
@@ -1,4 +1,4 @@
-const {normalizeURl} = require("./crawl.js")
+const {normalizeURl,getURLsfromHTML} = require("./crawl.js")
 const {test,expect}  = require("@jest/globals");
 
 test("normalizeURL stripProtocol",()=>{
@@ -22,3 +22,55 @@ test("normalizeURL Capital",()=>{
   expect(actual).toEqual(expected);
 })
 
+test("checkOutput HTMLBody link absolute",()=>{
+  const inputHTML=`
+  <html>
+  <body>
+  <a href="https://blog.boot.dev/path/">
+  Link
+  </a>
+  </body>
+  </html>
+  `
+  const baseURL = "https://blog.boot.dev/path/";
+  const actual =getURLsfromHTML(inputHTML,baseURL);
+  const expected = ["https://blog.boot.dev/path/"]
+  expect(actual).toEqual(expected);
+})
+
+test("checkOutput HTMLBody link relative&Absolute",()=>{
+  const inputHTML=`
+  <html>
+  <body>
+  <a href="/path1/">
+  Link1
+  </a>
+  <a href="https://blog.boot.dev/path2/">
+  Link2
+  </a>
+  </body>
+  </html>
+  `
+  const baseURL = "https://blog.boot.dev";
+  const actual =getURLsfromHTML(inputHTML,baseURL);
+  console.log(actual)
+  const expected = ["https://blog.boot.dev/path1/","https://blog.boot.dev/path2/"]
+  expect(actual).toEqual(expected);
+})
+
+test("checkOutput HTMLBody link relative&Absolute",()=>{
+  const inputHTML=`
+  <html>
+  <body>
+  <a href="invalid">
+  Invalid
+  </a>
+  </body>
+  </html>
+  `
+  const baseURL = "https://blog.boot.dev";
+  const actual =getURLsfromHTML(inputHTML,baseURL);
+  console.log(actual)
+  const expected = []
+  expect(actual).toEqual(expected);
+})
